868c88a01251091f63624c4a8c4d54f32f2841b7
Hamza Maach <hamzamaach56@gmail.com>
2024-07-16 14:17:33 +0100

    handele client errors (4xx

--- features/handlers.go
+++ features/handlers.go
@@ -8,11 +8,26 @@ import (
 func HandleAsciiArt(w http.ResponseWriter, r *http.Request, tmpl string) {
 	str := r.FormValue("string")
 	banner := r.FormValue("banner")
-	if CheckValidInput(str) || CheckBanner(banner) {
-		http.Error(w, "400 | Bad Request: Invalid input or banner type", http.StatusBadRequest)
+
+	if CheckBanner(banner) {
+		http.Error(w, "404 | Bad Request: Banner not found", http.StatusNotFound)
 		return
 	}
-	data := ProcessInput(str, banner)
+	if str == "" {
+		http.Error(w, "400 | Bad Request: No input provided", http.StatusBadRequest)
+		return
+	}
+
+	if CheckValidInput(str) {
+		http.Error(w, "400 | Bad Request: Invalid input. The input must contain characters with ASCII values ranging from 32 to 126.", http.StatusBadRequest)
+		return
+	}
+
+	data, err := ProcessInput(w, str, banner)
+	if err != nil {
+		http.Error(w, "500 | Internal Server Error !", http.StatusInternalServerError)
+		return
+	}
 	RenderTemplate(w, tmpl, data)
 }
 
@@ -22,8 +37,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.URL.Path {
 	case "/":
+		if r.Method != http.MethodGet {
+			http.Error(w, "405 | Method Not Allowed: Use GET", http.StatusMethodNotAllowed)
+			return
+		}
+
 		RenderTemplate(w, tmpl, nil)
 	case "/ascii-art":
+		if r.Method != http.MethodPost {
+			http.Error(w, "405 | Method Not Allowed: Use POST", http.StatusMethodNotAllowed)
+			return
+		}
 		HandleAsciiArt(w, r, tmpl)
 	default:
 		http.NotFound(w, r)
