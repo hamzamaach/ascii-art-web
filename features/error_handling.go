cc610972697b4f1e7c387deeea1165cfed379f04
Hamza Maach <hamzamaach56@gmail.com>
2024-07-16 14:19:01 +0100

    ignore new lines in the input validation

--- features/error_handling.go
+++ features/error_handling.go
@@ -1,5 +1,7 @@
 package ascii_art_web
 
+import "strings"
+
 func CheckBanner(banner string) bool {
 	switch banner {
 	case "shadow", "standard", "thinkertoy":
@@ -12,6 +14,8 @@ func CheckBanner(banner string) bool {
 
 // validates if the input contains only printable ASCII characters
 func CheckValidInput(input string) bool {
+	input = strings.ReplaceAll(input, "\r", "")
+	input = strings.ReplaceAll(input, "\n", "")
 	for _, char := range input {
 		if int(char) < 32 || int(char) > 126 {
 			return true
