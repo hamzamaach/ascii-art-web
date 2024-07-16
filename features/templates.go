aca320392c3cf347f6a9e6e25904451a2135cccb
Hamza Maach <hamzamaach56@gmail.com>
2024-07-16 14:15:33 +0100

    handele Internal Server Errors

--- features/templates.go
+++ features/templates.go
@@ -1,20 +1,25 @@
 package ascii_art_web
 
 import (
-    "fmt"
-    "html/template"
-    "net/http"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
 // RenderTemplate renders the specified template with data
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-    t, err := template.ParseFiles(tmpl)
-    if err != nil {
-        fmt.Println("Error: ", err)
-        return
-    }
-    err = t.ExecuteTemplate(w, tmpl, data)
-    if err != nil {
-        fmt.Println("Error executing template: ", err)
-    }
+
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		http.Error(w, "500 | Internal Server Error !", http.StatusInternalServerError)
+		fmt.Println("Error: ", err)
+		return
+	}
+    
+	err = t.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, "500 | Internal Server Error !", http.StatusInternalServerError)
+		fmt.Println("Error executing template: ", err)
+		return
+	}
 }
