df5d748fd16ee3a9982526224b3aa8f5418c5acf
Hamza Maach <hamzamaach56@gmail.com>
2024-07-18 16:49:29 +0100

    stylizing

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("./assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", ft.Handler)
 	fmt.Println("Starting the server on : http://localhost:8080")
 	http.ListenAndServe("localhost:8080", nil)
