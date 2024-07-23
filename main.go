4466970d26a004e887434d79738eaa46df792f6b
Hamza Maach <hamzamaach56@gmail.com>
2024-07-23 10:58:15 +0100

    Fixed bug: remove the listening on the localhost and keep the port only

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", ft.Handler)
 	fmt.Println("Starting the server on : http://localhost:8080")
-	err := http.ListenAndServe("localhost:8080", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("500 | Internal Server Error :", err)
 	}
