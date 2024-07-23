3a98911b76ad3e9ceace442e895595cf9a47d3cf
Hamza Maach <hamzamaach56@gmail.com>
2024-07-23 13:43:52 +0100

    create download function

--- features/handlers.go
+++ features/handlers.go
@@ -2,6 +2,7 @@ package ascii_art_web
 
 import (
 	"net/http"
+	"os"
 )
 
 type Data struct {
@@ -10,15 +11,34 @@ type Data struct {
 	Banner    string
 }
 
+// DownloadHandler handles the download of the ASCII art file
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Query().Get("file")
+	if file == "" {
+		http.Error(w, "400 | Bad Request: No file specified", http.StatusBadRequest)
+		return
+	}
+	content, err := os.ReadFile(file)
+	if err != nil {
+		http.Error(w, "404 | Not Found: File not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+file)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write(content)
+}
+
 // HandleAsciiArt processes the "ascii-art" route
 func HandleAsciiArt(w http.ResponseWriter, r *http.Request, tmpl string) {
 	str := r.FormValue("string")
 	banner := r.FormValue("banner")
+	export := r.FormValue("export")
 
 	if !ValidateInput(w, str, banner) {
 		return
 	}
-	
+
 	asciiArt, err := ProcessInput(w, str, banner)
 	if err != nil {
 		http.Error(w, "500 | Internal Server Error !", http.StatusInternalServerError)
@@ -30,7 +50,16 @@ func HandleAsciiArt(w http.ResponseWriter, r *http.Request, tmpl string) {
 		Input:     str,
 		Banner:    banner,
 	}
-	RenderTemplate(w, tmpl, data)
+
+	switch export {
+	case "download":
+		filename := "ascii_art.txt"
+		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(data.Ascii_art))
+	default:
+		RenderTemplate(w, tmpl, data)
+	}
 }
 
 // MainHandler handles the root route and other cases
